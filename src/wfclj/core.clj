b9c7520ea1cc9c1f0c77b33863c53d27e800c038
Martin Clausen <martin.clausen@gmail.com>
2020-08-01 00:04:28 +0200

    Clean up and added term-colors dependency (not used yet).

--- src/wfclj/core.clj
+++ src/wfclj/core.clj
@@ -83,22 +83,11 @@
 (defn fully-collapsed? [grid]
   (every? true? (map collapsed? (keys grid) (repeat grid))))
 
-(defn constrain [tiles1 tiles2]
-  (clojure.set/intersection tiles1 tiles2))
-
 (defn tiles-loc [loc grid]
   (first (get grid loc)))  
 
 (defn swap-possible-tiles [tiles loc grid]
   (assoc-in grid [loc 0] tiles))
-
-(defn constrain-dir [loc dir grid]
-  (let [val (collapsed-val loc grid)
-        loc-neighbour (neighbour loc dir)
-        possible-tiles (tiles-loc loc-neighbour)
-        allowed-tiles (get-in compabilities [val dir])
-        constrained-tiles (constrain possible-tiles allowed-tiles)]
-    (swap-possible-tiles constrained-tiles loc-neighbour grid)))
 
 ;; https://stackoverflow.com/questions/14464011/idiomatic-clojure-for-picking-between-random-weighted-choices
 (defn weighted-rand-choice [m]
@@ -140,24 +129,19 @@
   (update-in grid [loc 0] #(disj % tile)))
 
 (defn check-compatibility [tile other-tile dir]
-  (let [comps (get-in compabilities [tile dir]) 
-        res (contains? comps other-tile)]
-  ;;(println "checking if" other-tile " is compatible with " comps " for " tile " in direction " dir " result: " res)
-  res))
+  (let [comps (get-in compabilities [tile dir])]
+       (contains? comps other-tile)))
 
 (defn propagate [loc grid]
   (let [stack (atom [loc])
         grid (atom grid)]
     (while (not-empty @stack)
-      (println "Stack upon enter: " @stack)
       (let [[s l] [(pop @stack) (peek @stack)]
             cur-possible-tiles (tiles-loc l @grid)]
         (reset! stack s)
         (doall
          (for [direction (second (get @grid l))];; we get the precomputed valid-dirs
-           (let [n (neighbour l direction)
-                 ;;dummy (println "Considering " (second (get @grid l)) " directions, for location " l)
-                 ]
+           (let [n (neighbour l direction)]
              (doall
               (for [other-tile (tiles-loc n @grid)]
                 (let [other-tile-is-possible
@@ -167,18 +151,10 @@
                                        (repeat direction)))]
                   (when (not other-tile-is-possible)
                     (reset! grid (remove-from-possible-tiles other-tile n @grid))
-                    ;;(println "Removed: " other-tile " from " n " because " other-tile " is not compatible with any of " cur-possible-tiles "in current cell " l  " in direction " direction )
-                    ;;(when (= l n) (println "ERROR: " l " 0 " n))
-                    ;;(println "New contents of neighbour: " n " is " (get @grid n))
-                    (println "Adding: " n " to the stack.")
-                    (swap! stack conj n)
-                    (println "New stack: " @stack))))))))))
-    ;;(clojure.pprint/pprint @grid)
+                    (swap! stack conj n))))))))))
     @grid))
 
-
 (defn grid-iterator [grid]
-  ;; TODO implement iterator
   (let [[mel _] (min-entropy-loc grid)
         new-grid (collapse-loc mel grid)]
     (if (fully-collapsed? new-grid)
@@ -193,71 +169,7 @@
           (when (fully-collapsed? g)
             g))
         (iterate-grid grid)))
-(let [[cols rows] [3 3]
-      grid (make-grid cols rows  #{:s :l :c})]
-  (map clojure.pprint/pprint
-       (run grid)))
-
-
-(pop [:a :b :c])
 
 (print-grid
-(run (make-grid 10 10  #{:s :l :c}))
+ (run (make-grid 10 10  #{:s :l :c}))
 10)
-
-(clojure.pprint/pprint
- (propagate [1 1] (swap-possible-tiles #{:s} [1 1]  (make-grid 3 3 #{:s :l :c}))))
-     
-
-(map #(clojure.pprint/pprint (sort %))
-     (take 3 (iterate-grid test-grid)))
-
-
-(def test-grid
-  {[0 0] [#{:s} [[1 0] [0 1]]],
-   [0 1] [#{:s :c} [[1 0] [0 -1] [0 1]]],
-   [0 2] [#{:s :l :c} [[1 0] [0 -1]]],
-   [1 0] [#{:s} [[-1 0] [1 0] [0 1]]],
-   [1 1] [#{:s :l :c} [[-1 0] [1 0] [0 -1] [0 1]]]
-   [1 2] [#{:s :l :c} [[-1 0] [1 0] [0 -1]]],
-   [2 0] [#{:s :l :c} [[-1 0] [0 1]]],
-   [2 1] [#{:s :l :c} [[-1 0] [0 -1] [0 1]]],
-   [2 2] [#{:s :l :c} [[-1 0] [0 -1]]]})
-
-test-grid
-
-
-(check-compatibility :c :s [1 0])
-
-(clojure.pprint/pprint compabilities)
-(clojure.pprint/pprint
- (sort
-  (flat-compabilities input-matrix-1)))
-
-(defn alt-print-grid [grid cols]
-  (clojure.pprint/pprint
-   (for [row (partition cols grid)]
-     (map (fn [[_ [l _]]] l) row))))
-
-(defn alt-rules [matrix dims]
-  (into
-   #{}
-   (apply concat
-          (for [[x row] (map-indexed vector matrix)
-                [y tile] (map-indexed vector row)]
-            (for [[dx dy :as d] (valid-dirs [x y] dims)]
-              (let [other-tile (get-in matrix [(+ x dx) (+ y dy)])]
-                [tile other-tile d]))))))
-
-(defn flat-compabilities [matrix]
-  (let [dims (dimensions matrix)]
-         (alt-rules matrix dims)))
-
-(defn check-compatibility [tile other-tile dir]
-  (contains? 
-   (get-in compabilities [tile dir])
-   other-tile))
-
-(defn update-values [m f & args]
-  (into {} (for [[k v] m] [k (apply f v args)])))
-
